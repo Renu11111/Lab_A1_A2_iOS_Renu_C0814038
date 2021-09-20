01b9e436aacff83f6ce3bf665e32bdc01157efb8
Renu <C0814038@mylambton.ca>
2021-09-20 10:45:30 -0400

    Initial Commit

---
+++ Lab_A1_A2_iOS_Renu_C0814038/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_A1_A2_iOS_Renu_C0814038
+//
+//  Created by Mac on 2021-09-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
